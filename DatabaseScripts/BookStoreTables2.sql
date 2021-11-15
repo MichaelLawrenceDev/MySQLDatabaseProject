b4363acb5a59f7203cb8fdd6419f5bc659a7fefb
Michael Lawrence <michael@mikedevelops.com>
2021-11-14 21:08:03 -0600

    Added CASCADE ON DELETE or SET NULL to FKs

--- DatabaseScripts/BookStoreTables2.sql
+++ DatabaseScripts/BookStoreTables2.sql
@@ -1,29 +1,32 @@
 CREATE TABLE CMail(
 	Email varchar(40),
-	ContactID int foreign key references Contact_Details(ContactID),
+	ContactID int foreign key references Contact_Details(ContactID)
+		ON DELETE CASCADE,
 	primary key(Email, ContactID)
 );
 
 CREATE TABLE CAddress(
 	Address varchar(100),
-	ContactID int foreign key references Contact_Details(ContactID),
+	ContactID int foreign key references Contact_Details(ContactID)
+		ON DELETE CASCADE,
 	primary key(Address, ContactID)
 );
 
 CREATE TABLE CNumber(
 	Phone_Number int,
-	ContactID int foreign key references Contact_Details(ContactID),
+	ContactID int foreign key references Contact_Details(ContactID)
+		ON DELETE CASCADE,
 	primary key(Phone_Number, ContactID)
 );
 
 CREATE TABLE Assigned_to(
-	ISBN int foreign key references Books(ISBN),
+	ISBN bigint foreign key references Books(ISBN),
 	Category_Code int foreign key references Book_Categories(Category_Code),
 	primary key(ISBN, Category_Code)
 );
 
 CREATE TABLE Written_by(
-	ISBN int foreign key references Books(ISBN),
+	ISBN bigint foreign key references Books(ISBN),
 	AuthorID int foreign key references Author_of_the_books(AuthorID),
 	primary key(ISBN, AuthorID)
 );
