911474ae70f08ae07443bbf735ead86cf7ea4452
Michael Lawrence <michael@mikedevelops.com>
2021-11-14 21:08:40 -0600

    added ON DELETE SET NULL to FKs

--- DatabaseScripts/BookStoreTableForeignKeys.sql
+++ DatabaseScripts/BookStoreTableForeignKeys.sql
@@ -1,20 +1,34 @@
 ALTER TABLE Customer
-ADD ContactID int foreign key references Contact_Details(ContactID);
+ADD FK_ContactID int foreign key 
+references Contact_Details(ContactID)
+ON DELETE SET NULL;
 
 ALTER TABLE Author_of_the_books
-ADD ContactID int foreign key references Contact_Details(ContactID);
+ADD FK_ContactID int foreign key 
+references Contact_Details(ContactID)
+ON DELETE SET NULL;
 
 ALTER TABLE Orders
-ADD CustomerID int foreign key references Customer(CustomerID);
+ADD FK_CustomerID int foreign key
+references Customer(CustomerID)
+ON DELETE SET NULL;
 
 ALTER TABLE Books
-ADD SupplierID int foreign key references Supplier(SupplierID);
+ADD FK_SupplierID int foreign key 
+references Supplier(SupplierID)
+ON DELETE SET NULL;
 
 ALTER TABLE Order_Items
-ADD ISBN int foreign key references Books(ISBN);
+ADD FK_ISBN bigint foreign key 
+references Books(ISBN)
+ON DELETE SET NULL;
 
 ALTER TABLE Order_Items
-ADD OrderID int foreign key references Orders(OrderID);
+ADD FK_OrderID int foreign key 
+references Orders(OrderID)
+ON DELETE SET NULL;
 
 ALTER TABLE Supplier_Rep
-ADD SupplierID int foreign key references Supplier(SupplierID);
+ADD FK_SupplierID int foreign key 
+references Supplier(SupplierID)
+ON DELETE SET NULL;;
