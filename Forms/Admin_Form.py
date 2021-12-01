b2a09c10b4b1e07040c68c089bf7a6d7c8c31456
oluwabunmiariyo <93936849+oluwabunmiariyo@users.noreply.github.com>
2021-11-30 23:28:29 -0600

    Create Admin_Form.py

--- Forms/Admin_Form.py
+++ Forms/Admin_Form.py
@@ -66,21 +66,60 @@ def Remove(admin_cursor, username):
         admin_cursor.execute("delete from Customer where CustomerID=='"+ y_CustomerID.get() +"'")
         admin_cursor.execute('commit')
 
-        y_CustomerID.remove(0, 'end')
-        y_First_Name.remove(0, 'end')
-        y_Last_Name.remove(0, 'end')
-        y_ContactID.remove(0, 'end')
-        y_Username.remove(0, 'end')
-        y_Password.remove(0, 'end')
-        y_CustomerID.delete(0, 'end')
+        y_CustomerID.Remove(0, 'end')
+        y_First_Name.Remove(0, 'end')
+        y_Last_Name.Remove(0, 'end')
+        y_ContactID.Remove(0, 'end')
+        y_Username.Remove(0, 'end')
+        y_Password.Remove(0, 'end')
+        y_CustomerID.Remove(0, 'end')
         
+    def RemoveAuthor():
+        AuthorID = y_AuthorID.get()
+        First_Name = y_First_Name.get()
+        Last_Name = y_Last_Name.get()
+        Gender = y_Gender.get()
+        Date_of_Birth = y_Date_of_Birth.get()
+        ContactID = y_ContactID.get()
+        admin_cursor.execute("delete from Author_of_the_books where AuthorID=='"+ y_AuthorID.get() +"'")
+        admin_cursor.execute('commit')
         
-   
-
-# Entry into Admin_Form.py
-def Start(admin_conn, username):
+        y_AuthorID.Remove(0, 'end')
+        y_First_Name.Remove(0, 'end')
+        y_Last_Name.Remove(0, 'end')
+        y_Gender.Remove(0, 'end')
+        y_Date_of_Birth.Remove(0, 'end')
+        y_ContactID.Remove(0, 'end')
+        
+    def RemoveBook():
+        ISBN = y_ISBN.get()
+        Title = y_Title.get()
+        Publication_Date = y_Publication_Date.get()
+        Price = y_Price.get()
+        User_Reviews = y_User_Reviews.get()
+        SupplierID = y_SupplierID.get()
+        admin_cursor.execute("delete from Books where ISBN=='"+ y_ISBN.get() +"'")
+        admin_cursor.execute('commit')
+        
+        y_ISBN.Remove(0, 'end')
+        y_Title.Remove(0, 'end')
+        y_Publication_Date.Remove(0, 'end')
+        y_Price.Remove(0, 'end')
+        y_User_Reviews.Remove(0, 'end')
+        y_SupplierID.Remove(0, 'end')
+        
+    def RemoveSupplier():
+        SupplierID = y_SupplierID.get()
+        Supplier_Name = y_Supplier_Name.get()
+        admin_cursor.execute("delete from Supplier where SupplierID=='"+ y_SupplierID.get() +"'")
+        admin_cursor.execute('commit')
+        
+        y_SupplierID.Remove(0, 'end')
+        y_Supplier_Name.Remove(0, 'end')
+       
+# Entry into Admin_Form.py and view all customer
+def Start(admin_cursor, username):
     # cursor should already be connected.
-    admin_cursor = admin_conn.cursor()
     print("Logging in as admin under username: " + username)
     admin_cursor.execute("select * from Customer")
     print(list(admin_cursor))
